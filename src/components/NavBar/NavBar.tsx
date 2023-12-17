b7d6f9d5283f66615db50d39932784ac9186b6ae
kfac <kfacison1998@gmail.com>
2023-12-16 23:55:49 -0600

    added styling and modles files"

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import './NavBar.css';
+
+
 export default function NavBar(){
     return (
-        <p>
-            p
-        </p>
+        <div>
+            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/Slimes" className="nav-links">Slimes</Link>
+            <Link to="/Foods" className="nav-links">Foods</Link>
+        </div>
     );
 }
