a5a09a074ed1a6e87b17de1819708f80f2264bc5
kfac <kfacison1998@gmail.com>
2023-12-17 19:54:38 -0600

    updated ERD

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,8 +6,9 @@ export default function NavBar(){
     return (
         <div>
             <Link to="/" className="nav-links">Home</Link>
-            <Link to="/Slimes" className="nav-links">Slimes</Link>
-            <Link to="/Foods" className="nav-links">Foods</Link>
+            <Link to="/slimes" className="nav-links">Slimes</Link>
+            <Link to="/foods" className="nav-links">Foods</Link>
+            <Link to="/about" className="nav-links">About</Link>
         </div>
     );
 }
